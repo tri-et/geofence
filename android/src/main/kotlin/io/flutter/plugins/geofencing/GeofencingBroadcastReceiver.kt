7e8a35359e8b3d7b71158765d8fefcc7cdf5815e
John <tripham178@gmail.com>
2019-04-03 13:12:34 +0700

    testing

---
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingBroadcastReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.geofencing
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import io.flutter.view.FlutterMain
+
+
+class GeofencingBroadcastReceiver : BroadcastReceiver() {
+    companion object {
+        private const val TAG = "GeofencingBroadcastReceiver"
+    }
+    override fun onReceive(context: Context, intent: Intent) {
+        FlutterMain.ensureInitializationComplete(context, null)
+        GeofencingService.enqueueWork(context, intent)
+    }
+}
